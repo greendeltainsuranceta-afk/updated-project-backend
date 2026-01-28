aa4b69a1c9fc831d9c5362c97acebd69abe5b216
unknown <mdfaisald040@gmail.com>
2026-01-28 15:12:09 +0600

    U

--- index.js
+++ index.js
@@ -209,7 +209,7 @@ app.post("/api/pdf/upload", upload.single("pdf"), async (req, res) => {
   const pdfObj = {
     id,
     filename: req.file.originalname,
-    url: `https://www.tunesprotect.com${port}/public/pdfs/${req.file.filename}`,
+    url: `https://www.tunesprotect.com/public/pdfs/${req.file.filename}`,
     createdAt: new Date(),
   };
 
